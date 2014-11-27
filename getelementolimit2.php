9354f8a36160c6ac22a0ff8dc48bd645c16f923e
infoutil20 <infoutil20@thomas-mckean.dreamhost.com>
2014-11-27 07:07:07 -0800

    solucion a escuelas

---
+++ getelementolimit2.php
@@ -0,0 +1,197 @@
+
+<?php
+/*Servicio que devuelve la lista de subcategorias
+recibe como parametros: el nombre de la tabla de la sub categoría.
+
+*/	
+
+    include 'conexion.php';
+
+    //header('Content-Type: text/json');
+
+	$idsubcat=$_REQUEST['idsubcat'];
+	$limit=$_REQUEST['limit'];
+	
+	$respuesta=array('resultado'=>2);
+	json_encode($respuesta);
+	$conexion=mysql_connect($servidor,$usuario,$password) or
+	die ("Problemas en la conexion");
+
+	mysql_select_db($baseDatos,$conexion)
+	or die("Problemas en la seleccion de la base de datos");
+
+$tabla = array(
+    2  => "medicines",
+    3  => "health_establishments",
+    4  => "doctors",
+    15 => "food_establishments",
+    6  => "schools",
+    7  => "universities",
+    9  => "product_categories",
+    12  => "finances",
+    14  => "product_categories",
+    19  => "companies",
+    21  => "product_categories",
+    26  => "solvent_companies",
+    27  => "electricity_companies",//por el momento solo envia el nombre de las compañias
+//    35  => "accountants",
+    36  => "accountants",
+    39  => "fodes_cities_transfers",
+    11  => "civil_organizations",
+    13  => "delegations",
+    16  => "syndicates",
+    17  => "cooperatives",
+    18  => "delation_institutions",
+    20  => "refuges",
+    25  => "risk_prevention_consultants",
+    28  => "telephone_companies_concessions",
+    29  => "radial_frequencies",
+    30  => "radial_concessions",
+    32  => "roads",
+    33  => "disabled_associations",
+    34  => "fovial_companies",
+    37  => "woman_cities",
+    41  => "municipality_infos",
+    23  => "sports_grants",
+    24  => "sports_federation_transfers",
+    31  => "cultural_fees",//ESTA TABLA NO EXISTE//se cambia cultural_events por cultural_fees para mostrarlos todos
+    38  => "cultural_fees",
+    42  => "natives",
+    43  => "libraries",
+    44  => "festivities",//ESTA TABLA TAMPOCO ESTA EN LA BASE PROPORCIONADA
+    45  => "choirs",
+    46  => "cultures"
+);
+
+
+switch ($idsubcat) {
+    case '14':
+        $registros=mysql_query("SELECT id, name FROM $tabla[$idsubcat] WHERE category_id=14 limit $limit, 10", $conexion) or
+        die(json_encode($respuesta));
+
+        $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+        echo json_encode($filas);
+    
+
+    break;
+    case '9':
+        $registros=mysql_query("SELECT id, name FROM $tabla[$idsubcat] where category_id=9 limit $limit, 10", $conexion) or
+        die(json_encode($respuesta));
+        $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+        echo json_encode($filas);
+    
+
+    break;
+    case '21':
+         $registros=mysql_query("SELECT id, name FROM $tabla[$idsubcat] where category_id=21 limit $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+         echo json_encode($filas);
+    
+
+    break;	
+    case '7':
+         $registros=mysql_query("SELECT id, name, address FROM $tabla[$idsubcat] limit $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+         echo json_encode($filas);
+    
+
+    break;
+     case '31':
+         $registros=mysql_query("SELECT id, description FROM $tabla[$idsubcat] limit $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+         echo json_encode($filas);
+    
+
+    break;
+     case '38':
+         $registros=mysql_query("SELECT id, description FROM $tabla[$idsubcat] limit $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+         echo json_encode($filas);
+    
+
+    break;
+     case '9':
+         $registros=mysql_query("SELECT id, description FROM $tabla[$idsubcat] limit $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+         echo json_encode($filas);
+    
+
+    break;
+      case '6':
+         $registros=mysql_query("SELECT schools.id, schools.name, school_infos.year, academic_grades.name as gradoAcademico, school_infos.turn, school_infos.quota 
+                                from schools 
+                                inner join school_infos on schools.id = school_infos.school_id 
+                                inner join academic_grades on school_infos.academic_grade_id=academic_grades.id  
+                                limit $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                
+                $filas[]=array_map('utf8_encode', $reg);
+
+            }
+            
+            $filas2 = array();
+            $filas3= array();
+            foreach ($filas as $key => $value) {
+                    # code...
+                $cadena=$value['name'].' '.$value['gradoAcademico'].' '.$value['year'].' '.$value['turn'].' '.$value['quota'];
+                $filas2['id']=$value['id'];
+                $filas2['name']=$cadena;
+                $filas3[]=$filas2;
+                /*array_push($filas2,$value['id'] );
+                array_push($filas2, $cadena);*/
+            }
+
+         echo json_encode($filas3);
+    break;
+    default:
+         $registros=mysql_query("SELECT id, name FROM $tabla[$idsubcat] limit $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+          $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+         echo json_encode($filas);
+	   
+	}
+
+
+
+	mysql_close($conexion);
+?>
---
+++ getperfillimit3.php
@@ -0,0 +1,230 @@
+<?php
+/*Servicio que devuelve la lista de subcategorias
+recibe como parametros: el nombre de la tabla de la sub categoría.
+
+*/	
+
+    include 'conexion.php';
+
+    //header('Content-Type: text/json');
+	
+    $idsubcat=$_REQUEST['idsubcat'];
+	$idcat=$_REQUEST['idcat'];
+	$idelemento=$_REQUEST['idelemento'];
+	$tipo=$_REQUEST['tipo'];
+	$limit=$_REQUEST['limit'];
+	
+	$respuesta=array('resultado'=>2);
+	json_encode($respuesta);
+
+	$conexion=mysql_connect($servidor,$usuario,$password) or
+	die ("Problemas en la conexion");
+
+    //mysql_set_charset('utf8');
+
+	mysql_select_db($baseDatos,$conexion)
+	or die("Problemas en la seleccion de la base de datos");
+
+$tabla = array(
+    2  => "medicines",
+    3  => "health_establishments",
+    4  => "doctors",
+    15 => "food_establishments",
+    6  => "schools",
+    7  => "universities",
+    9  => "product_categories",
+    12  => "finances",
+    14  => "product_categories",
+    19  => "companies",
+    21  => "product_categories",
+    26  => "solvent_companies",
+    27  => "electricity_companies",//por el momento solo envia el nombre de las compañias
+//    35  => "accountants",
+    36  => "accountants",
+    39  => "fodes_cities_transfers",
+    11  => "civil_organizations",
+    13  => "delegations",
+    16  => "syndicates",
+    17  => "cooperatives",
+    18  => "delation_institutions",
+    20  => "refuges",
+    25  => "risk_prevention_consultants",
+    28  => "telephone_companies_concessions",
+    29  => "radial_frequencies",
+    30  => "radial_concessions",
+    32  => "roads",
+    33  => "disabled_associations",
+    34  => "fovial_companies",
+    37  => "woman_cities",
+    41  => "municipality_infos",
+    23  => "sports_grants",
+    24  => "sports_federation_transfers",
+    31  => "cultural_fees",//ESTA TABLA NO EXISTE//se cambia cultural_events por cultural_fees para mostrarlos todos
+    38  => "cultural_fees",
+    42  => "natives",
+    43  => "libraries",
+    44  => "festivities",//ESTA TABLA TAMPOCO ESTA EN LA BASE PROPORCIONADA
+    45  => "choirs",
+    46  => "cultures"
+);
+        mysql_query("INSERT INTO ELEMENTO SET VISITAS=1, IDELEMENTO=$idelemento, IDSUBCATEGORIA=$idsubcat, IDCATEGORIA=$idcat, CANTCOMENTARIOS=0, PROMEVALUACION=0, DENUNCIAS=0 ON DUPLICATE KEY UPDATE VISITAS=VISITAS+1", $conexion) or die(json_encode($respuesta));
+
+	switch ($idsubcat) {
+    case '2':
+       //        mysql_query("UPDATE ELEMENTO SET VISITAS=VISITAS+1 WHERE IDSUBCATEGORIA=$idsubcat AND IDELEMENTO=$idelemento", $conexion) or die(json_encode($respuesta));
+
+
+        $registros=mysql_query("SELECT id, name, quantity, unit, price  FROM $tabla[$idsubcat]  WHERE  id=$idelemento ", $conexion) or
+        die(json_encode($respuesta));
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+             {
+                $filas[]=array_map('utf8_encode', $reg);
+             }
+
+        $registros2=mysql_query("SELECT ELEMENTO.PROMEVALUACION, COMENTARIO.NOMBREUSUARIO, COMENTARIO.COMENTARIO, COMENTARIO.EVALUACION, COMENTARIO.FECHA FROM ELEMENTO, COMENTARIO WHERE ELEMENTO.IDELEMENTO=$idelemento AND COMENTARIO.IDCATEGORIA=$idcat AND COMENTARIO.IDSUBCATEGORIA=$idsubcat AND COMENTARIO.IDELEMENTO=$idelemento AND COMENTARIO.TIPO=$tipo ORDER BY COMENTARIO.FECHA DESC  LIMIT $limit, 10", $conexion) or
+        die(json_encode($respuesta));
+
+        $filas2=array();
+            while ($reg2=mysql_fetch_assoc($registros2))
+             {
+                $filas2[]=array_map('utf8_encode', $reg2);
+             }
+
+        $descripcion=array('id'=>'id','name'=>'nombre','cantidad'=>'cantidad','unidad'=>'unidad','precio'=>'Precio máximo de venta');      
+
+        $answ=array('Elemento'=>$filas,'Comentarios'=>$filas2,'Descripcion'=>$descripcion);
+            echo json_encode($answ);
+    
+
+    break;
+    case '3':
+        $registros=mysql_query("SELECT id, name, phone, address FROM $tabla[$idsubcat] WHERE $tabla[$idsubcat].id=$idelemento", $conexion) or
+        die(json_encode($respuesta));
+        
+        $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+             {
+                $filas[]=array_map('utf8_encode', $reg);
+             }
+        $registros2=mysql_query("SELECT ELEMENTO.PROMEVALUACION, COMENTARIO.NOMBREUSUARIO, COMENTARIO.COMENTARIO, COMENTARIO.EVALUACION, COMENTARIO.FECHA FROM ELEMENTO, COMENTARIO WHERE ELEMENTO.IDELEMENTO=$idelemento AND COMENTARIO.IDCATEGORIA=$idcat AND COMENTARIO.IDSUBCATEGORIA=$idsubcat AND COMENTARIO.IDELEMENTO=$idelemento AND COMENTARIO.TIPO=$tipo ORDER BY COMENTARIO.FECHA DESC  LIMIT $limit, 10", $conexion) or
+        die(json_encode($respuesta));
+
+        $filas2=array();
+            while ($reg2=mysql_fetch_assoc($registros2))
+             {
+                $filas2[]=array_map('utf8_encode', $reg2);
+             }
+        $descripcion=array('id'=>'id','name'=>'nombre','telefono'=>'teléfono','direccion'=>'dirección');      
+
+         $answ=array('Elemento'=>$filas,'Comentarios'=>$filas2,'Descripcion'=>$descripcion);
+            echo json_encode($answ);
+    
+
+    break;
+    case '4':
+        $registros=mysql_query("SELECT $tabla[$idsubcat].id, $tabla[$idsubcat].name, $tabla[$idsubcat].register_number, doctor_especialities.name as carrera FROM $tabla[$idsubcat], doctor_especialities WHERE $tabla[$idsubcat].id=$idelemento AND $tabla[$idsubcat].doctor_especiality_id=doctor_especialities.id ", $conexion) or
+        die(json_encode($respuesta));
+        
+        $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+             {
+                $filas[]=array_map('utf8_encode', $reg);
+             }
+
+        $registros2=mysql_query("SELECT ELEMENTO.PROMEVALUACION, COMENTARIO.NOMBREUSUARIO, COMENTARIO.COMENTARIO, COMENTARIO.EVALUACION, COMENTARIO.FECHA FROM ELEMENTO, COMENTARIO WHERE ELEMENTO.IDELEMENTO=$idelemento AND COMENTARIO.IDCATEGORIA=$idcat AND COMENTARIO.IDSUBCATEGORIA=$idsubcat AND COMENTARIO.IDELEMENTO=$idelemento AND COMENTARIO.TIPO=$tipo ORDER BY COMENTARIO.FECHA DESC  LIMIT $limit, 10", $conexion) or
+        die(json_encode($respuesta));
+
+        $filas2=array();
+            while ($reg2=mysql_fetch_assoc($registros2))
+             {
+                $filas2[]=array_map('utf8_encode', $reg2);
+             }
+
+        $descripcion=array('id'=>'id','name'=>'nombre','Numero de Registro'=>'Numero de Registro','carrera'=>'carrera');      
+
+         $answ=array('Elemento'=>$filas,'Comentarios'=>$filas2,'Descripcion'=>$descripcion);
+            echo json_encode($answ);
+    
+
+    break;
+    case '15':
+        $registros=mysql_query("SELECT id, name, address, authorized_at, authorization_expire_at FROM $tabla[$idsubcat] WHERE $tabla[$idsubcat].id=$idelemento", $conexion) or
+        die(json_encode($respuesta));
+        
+        $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+             {
+                $filas[]=array_map('utf8_encode', $reg);
+             }
+
+        $registros2=mysql_query("SELECT ELEMENTO.PROMEVALUACION, COMENTARIO.NOMBREUSUARIO, COMENTARIO.COMENTARIO, COMENTARIO.EVALUACION, COMENTARIO.FECHA FROM ELEMENTO, COMENTARIO WHERE ELEMENTO.IDELEMENTO=$idelemento AND COMENTARIO.IDCATEGORIA=$idcat AND COMENTARIO.IDSUBCATEGORIA=$idsubcat AND COMENTARIO.IDELEMENTO=$idelemento AND COMENTARIO.TIPO=$tipo ORDER BY COMENTARIO.FECHA DESC  LIMIT $limit, 10", $conexion) or
+        die(json_encode($respuesta));
+
+        $filas2=array();
+            while ($reg2=mysql_fetch_assoc($registros2))
+             {
+                $filas2[]=array_map('utf8_encode', $reg2);
+             }
+
+        $descripcion=array('id'=>'id','name'=>'nombre','direccion'=>'Dirección','Fecha de Autorizacion'=>'Fecha de Autorización','Fecha expiracion de Autorizacion'=>'Fecha expiración de Autorización');      
+
+         $answ=array('Elemento'=>$filas,'Comentarios'=>$filas2,'Descripcion'=>$descripcion);
+            echo json_encode($answ);
+    
+
+    break;
+     case '6':
+        $registros=mysql_query("SELECT id, name, address, authorized_at, authorization_expire_at FROM $tabla[$idsubcat] WHERE $tabla[$idsubcat].id=$idelemento", $conexion) or
+        die(json_encode($respuesta));
+        
+        $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+             {
+                $filas[]=array_map('utf8_encode', $reg);
+             }
+
+        $registros2=mysql_query("SELECT ELEMENTO.PROMEVALUACION, COMENTARIO.NOMBREUSUARIO, COMENTARIO.COMENTARIO, COMENTARIO.EVALUACION, COMENTARIO.FECHA FROM ELEMENTO, COMENTARIO WHERE ELEMENTO.IDELEMENTO=$idelemento AND COMENTARIO.IDCATEGORIA=$idcat AND COMENTARIO.IDSUBCATEGORIA=$idsubcat AND COMENTARIO.IDELEMENTO=$idelemento AND COMENTARIO.TIPO=$tipo ORDER BY COMENTARIO.FECHA DESC  LIMIT $limit, 10", $conexion) or
+        die(json_encode($respuesta));
+
+        $filas2=array();
+            while ($reg2=mysql_fetch_assoc($registros2))
+             {
+                $filas2[]=array_map('utf8_encode', $reg2);
+             }
+
+        $descripcion=array('id'=>'id','name'=>'nombre','direccion'=>'Dirección','Fecha de Autorizacion'=>'Fecha de Autorización','Fecha expiracion de Autorizacion'=>'Fecha expiración de Autorización');      
+
+         $answ=array('Elemento'=>$filas,'Comentarios'=>$filas2,'Descripcion'=>$descripcion);
+            echo json_encode($answ);
+    
+
+    break;
+    default:
+         $registros=mysql_query("SELECT id, name FROM $tabla[$idsubcat] WHERE $tabla[$idsubcat].id=$idelemento ", $conexion) or
+         die(json_encode($respuesta));
+
+         $filas=array();
+            while ($reg=mysql_fetch_assoc($registros))
+            {
+                $filas[]=array_map('utf8_encode', $reg);
+            }
+         
+         $registros2=mysql_query("SELECT ELEMENTO.PROMEVALUACION, COMENTARIO.NOMBREUSUARIO, COMENTARIO.COMENTARIO, COMENTARIO.EVALUACION, COMENTARIO.FECHA FROM ELEMENTO, COMENTARIO WHERE ELEMENTO.IDELEMENTO=$idelemento AND COMENTARIO.IDCATEGORIA=$idcat AND COMENTARIO.IDSUBCATEGORIA=$idsubcat AND COMENTARIO.IDELEMENTO=$idelemento AND COMENTARIO.TIPO=$tipo ORDER BY COMENTARIO.FECHA DESC  LIMIT $limit, 10", $conexion) or
+         die(json_encode($respuesta));
+
+        $filas2=array();
+            while ($reg2=mysql_fetch_assoc($registros2))
+             {
+                $filas2[]=array_map('utf8_encode', $reg2);
+             }
+        $descripcion=array('id'=>'id','name'=>'nombre');      
+
+         $answ=array('Elemento'=>$filas,'Comentarios'=>$filas2,'Descripcion'=>$descripcion);
+            echo json_encode($answ);
+       
+    }
+
+	mysql_close($conexion);
+?>
+
